55f4fca002db3ad611c791eba7640e9e57044052
Andres <andres.guzman.cordero@hotmail.com>
2023-03-04 10:39:50 +0100

    Fixed the problem with the select_order function

--- Code A1/Assign1.py
+++ Code A1/Assign1.py
@@ -32,15 +32,10 @@ def ProblemB(input_df: pd.DataFrame):
     return
 
 
-def select_order(input_df: pd.DataFrame, lags: list):
-    models = []
+def select_order(models: list):
     output = dict()
-    for i in range(len(lags)):
-        Mod = VAR_Model(input_df, lags[i])
-        Mod.drop_obs(no_to_drop=(max(lags) - lags[i]))
-        models.append(Mod)
-        models[i].regress()
-        output["VAR(" + str(lags[i]) + ")"] = [models[i].AIC, models[i].SIC, models[i].HQIC]
+    for model in models:
+        output["VAR(" + str(model.order) + ")"] = [model.aic, model.sic, model.hqic]
     return output
 
 
@@ -55,7 +50,8 @@ class VAR_Model:
         self.order = lags
 
     def drop_obs(self, no_to_drop: int):
-        self.exog = self.exog.iloc[no_to_drop - 1:, :]
+        inter = self.exog.iloc[no_to_drop - 1:, :]
+        self.exog = inter
         return
 
     def regress(self):
@@ -72,17 +68,21 @@ class VAR_Model:
         col_num = self.exog.shape[1]
         self.coef = np.linalg.inv(exo[:, col_num:].T @ exo[:, col_num:]) @ exo[:, col_num:].T @ exo[:, :col_num]
         print(f'The following are the coefficients of the regression: \nC =  {self.coef[0, :]}')
-        row = 1
 
+        row = 1
         for i in range(1, self.order + 1):
             print(f'B_{i} = {self.coef[row:row + len(self.exog.columns), :]}')
             row += len(self.exog.columns)
+
         print("\n")
         self.pred = (exo[:, col_num:] @ self.coef)
+        self.AIC()
+        self.SIC()
+        self.HQIC()
         return
 
     def AIC(self):
-        if pred is None:
+        if self.pred is None:
             raise Exception(f'The model has not been estimated')
         else:
             n, p = self.exog.to_numpy().shape[0] - self.order, self.exog.to_numpy().shape[1]
@@ -90,11 +90,10 @@ class VAR_Model:
             resid = self.exog.iloc[self.order:, :] - self.pred
             sigma2 = (1 / n) * (resid.T @ resid)
             self.aic = np.log(np.linalg.det(sigma2)) + (2 * k) / n
-            print(f'The Akaike Information Criteria is: {self.aic}')
         return
 
     def SIC(self):
-        if pred is None:
+        if self.pred is None:
             raise Exception(f'The model has not been estimated')
         else:
             n, p = self.exog.to_numpy().shape[0] - self.order, self.exog.to_numpy().shape[1]
@@ -102,11 +101,10 @@ class VAR_Model:
             resid = self.exog.iloc[self.order:, :] - self.pred
             sigma2 = (1 / n) * (resid.T @ resid)
             self.sic = np.log(np.linalg.det(sigma2)) + (k * np.log(n)) / n
-            print(f'The Schwartz Information Criteria is: {self.sic}')
         return
 
     def HQIC(self):
-        if pred is None:
+        if self.pred is None:
             raise Exception(f'The model has not been estimated')
         else:
             n, p = self.exog.to_numpy().shape[0] - self.order, self.exog.to_numpy().shape[1]
@@ -114,7 +112,6 @@ class VAR_Model:
             resid = self.exog.iloc[self.order:, :] - self.pred
             sigma2 = (1 / n) * (resid.T @ resid)
             self.hqic = np.log(np.linalg.det(sigma2)) + (2 * k * np.log(np.log(n))) / n
-            print(f'The Hannan-Quinn Information Criteria is: {self.hqic}')
         return
 
     # def impulse_response(self):
@@ -124,7 +121,19 @@ class VAR_Model:
 def ProblemC(input_df: pd.DataFrame):
     exog_df = input_df.copy()
     exog_df.drop(columns='cpi', inplace=True)
-    order_res = select_order(input_df=exog_df, lags=[1, 2, 3])
+
+    model_1 = VAR_Model(exog_df, lags=1)
+    model_1.drop_obs(no_to_drop=2)
+    model_1.regress()
+
+    model_2 = VAR_Model(exog_df, lags=2)
+    model_2.drop_obs(no_to_drop=1)
+    model_2.regress()
+
+    model_3 = VAR_Model(exog_df, lags=3)
+    model_3.regress()
+
+    order_res = select_order(models=[model_1, model_2, model_3])
     print(order_res)
     return
 
